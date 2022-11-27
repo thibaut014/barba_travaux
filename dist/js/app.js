55343e165700b3064681e2e868f304f2ea5fb2ac
thibaut piquet <tib@MBP-de-thibaut.lan>
2022-11-27 19:17:48 +0100

    first commit

---
+++ dist/js/app.js
@@ -0,0 +1,55 @@
+// import barba from '@barba/core';
+// import barbaCss from '@barba/css';
+
+// tell Barba to use the css plugin
+barba.use(barbaCss);
+
+const body = document.querySelector('body');
+
+barba.hooks.before((data) => {
+
+    const background = data.current.container.dataset.background;
+    body.style.setProperty('--page-background' , background)
+
+});
+
+// init Barba
+barba.init({
+    transitions : [
+        {
+            name : 'home',
+            to: {
+                namespace: ['home']
+            },
+            sync : true ,
+            once (){},
+            leave() {},
+            enter() {}
+
+        }, {
+            name : 'fade',
+            to: {
+                namespace: ['fade']
+            },
+            leave() {},
+            enter() {}
+        }, {
+            name : 'clip',
+            sync: true ,
+            to: {
+                namespace: ['clip']
+            },
+            leave() {},
+            enter() {},
+            
+        }, {
+            name : 'with-cover',
+            to: {
+                namespace: ['with-cover']
+            },
+            leave() {},
+            enter() {},
+            
+        }
+    ]
+});
